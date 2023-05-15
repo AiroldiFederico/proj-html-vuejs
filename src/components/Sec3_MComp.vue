50a29da8cc10c23373aa6fc7c6c047da06006f50
Federico Airoldi <airoldibusiness@gmail.com>
2023-05-15 12:34:41 +0200

    FASE 4 - sezione 3 main conclusa

--- src/components/Sec3_MComp.vue
+++ src/components/Sec3_MComp.vue
@@ -11,7 +11,14 @@
         data(){
             return{
                
-
+                images: [
+                { path: '../../public/img/project-1.jpg', text: 'Academic professional program in social media' },
+                { path: '../../public/img/project-2.jpg', text: "President's speech at the annual meeting" },
+                { path: '../../public/img/project-3.jpg', text: 'International business trip in Shanghai' },
+                { path: '../../public/img/project-4.jpg', text: 'Technology workshop with education theme' },
+                { path: '../../public/img/project-5.jpg', text: 'Donation of clothes and food to the partner NGO' },
+                { path: '../../public/img/project-6.jpg', text: 'Confraternization of the procurement team' }
+                ]
 
             }
         },
@@ -43,7 +50,14 @@
             </ul>
 
             <!-- cards -->
-            <div>
+            <div id="id3">
+
+                <div class="card text-bg-dark" v-for="(img, index) in images" key="index">
+                    <img :src="img.path" class="card-img" alt="...">
+                    <div class="card-img-overlay card-body">
+                      <h4 class="card-title">{{img.text}}</h4>
+                    </div>
+                </div>
 
             </div>
 
@@ -60,7 +74,7 @@
 
     #Sec3_MComp {
       background-color: #111117;
-      padding: 5rem;
+      padding: 7rem 0rem;
 
       #core {
 
@@ -95,11 +109,39 @@
             }
         }
 
+        #id3 {
+
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            gap: 2rem;
+            margin-top: 2rem;
+
+            .card {
+    
+                width: calc(90% / 3);
+
+                img {
+                    filter: brightness(0.4);
+                }
+
+                .card-body {
+                    margin-top: 17rem;
+                }
+
+                h4 {
+                    font-weight: bold;
+                    font-size: 1.8rem;
+                }
+
+            }
+        }
+
       }
     }
 
     .green-bg {
-        background-color: #03848456;
+        background-color: #03848431;
         border-radius: 5px;
     }
 
