510879eb5739968f2fa4caa3ed0155ec4179cf54
Federico Airoldi <airoldibusiness@gmail.com>
2023-05-16 16:53:47 +0200

    FASE 8 - plus: rendo le cards flipcards

--- src/components/Sec3_MComp.vue
+++ src/components/Sec3_MComp.vue
@@ -12,16 +12,23 @@
             return{
                
                 images: [
-                { path: '../../public/img/project-1.jpg', text: 'Academic professional program in social media' },
-                { path: '../../public/img/project-2.jpg', text: "President's speech at the annual meeting" },
-                { path: '../../public/img/project-3.jpg', text: 'International business trip in Shanghai' },
-                { path: '../../public/img/project-4.jpg', text: 'Technology workshop with education theme' },
-                { path: '../../public/img/project-5.jpg', text: 'Donation of clothes and food to the partner NGO' },
-                { path: '../../public/img/project-6.jpg', text: 'Confraternization of the procurement team' }
+                { path: '../../public/img/project-1.jpg', text: 'Academic professional program in social media', flipped: false, text2:"Master social media marketing with our professional academic program. Learn the latest strategies, gain insights from industry experts, and elevate your career in this dynamic field." },
+                { path: '../../public/img/project-2.jpg', text: "President's speech at the annual meeting", flipped: false, text2:"Be inspired by the President's annual meeting speech. Gain valuable insights into our achievements, future goals, and strategic vision. Join us for a memorable event that shapes our organization's success." },
+                { path: '../../public/img/project-3.jpg', text: 'International business trip in Shanghai', flipped: false, text2:"Join our international business trip to Shanghai. Explore the dynamic business environment, build valuable connections, and discover new opportunities in this global economic hub." },
+                { path: '../../public/img/project-4.jpg', text: 'Technology workshop with education theme', flipped: false, text2:"Join our technology workshop with an education theme to explore cutting-edge innovations and transformative approaches in the field of learning. Engage with industry experts." },
+                { path: '../../public/img/project-5.jpg', text: 'Donation of clothes and food to the partner NGO', flipped: false, text2:"Support our partner NGO by donating clothes and food. Make a difference in the lives of those in need and bring hope to our community." },
+                { path: '../../public/img/project-6.jpg', text: 'Confraternization of the procurement team', flipped: false, text2:"Unite and celebrate with our procurement team. Join us for a joyful confraternization event to strengthen relationships and recognize the outstanding contributions of our dedicated professionals." }
                 ]
 
             }
         },
+
+        methods: {
+            flipCard(index) {
+                this.images[index].flipped = !this.images[index].flipped;
+            }
+        }
+
     }
     
 </script>
@@ -52,10 +59,18 @@
             <!-- cards -->
             <div id="id3">
 
-                <div class="card text-bg-dark" v-for="(img, index) in images" key="index">
-                    <img :src="img.path" class="card-img" alt="...">
-                    <div class="card-img-overlay card-body">
-                      <h4 class="card-title">{{img.text}}</h4>
+                <div class="card text-bg-dark" v-for="(img, index) in images" :key="index" v-bind:class="{ 'flip': img.flipped }" @mouseenter="flipCard(index)" @mouseleave="flipCard(index)">
+                    <div class="card-inner">
+                      <div class="card-front">
+                        <img :src="img.path" class="card-img" alt="img">
+                        <h4 class="card-title">{{ img.text }}</h4>
+                      </div>
+                      <div class="card-back">
+                        <div class="text-card-back">
+                            <h4 class="card-title">{{ img.text }}</h4>
+                            <p>{{ img.text2 }}</p>
+                        </div>
+                      </div>
                     </div>
                 </div>
 
@@ -92,9 +107,9 @@
             margin-top: 1rem;
 
             span {
-                background-color: #03848456;
+                background-color: #0384843a;
                 padding: 0rem 1rem;
-                border-radius: 10px;
+                border-radius: 5px;
             }
         }
 
@@ -113,25 +128,94 @@
 
             display: flex;
             flex-wrap: wrap;
-            justify-content: space-between;
-            gap: 2rem;
-            margin-top: 2rem;
+            justify-content: space-evenly;
+            margin-top: 2.5rem;
+            height: 50rem;
+   
 
             .card {
     
-                width: calc(90% / 3);
+                width: calc(80% / 3);
+                position: relative;
+                border-radius: 0.7rem;
+                aspect-ratio: 1;
+                height: 22.6rem;
 
                 img {
                     filter: brightness(0.4);
+                    border-radius: 0.7rem;
                 }
 
                 .card-body {
                     margin-top: 17rem;
                 }
 
-                h4 {
-                    font-weight: bold;
-                    font-size: 1.8rem;
+                .card-inner {
+                    position: relative;
+                    width: 100%;
+                    height: 100%;
+                    transition: transform 0.6s;
+                    transform-style: preserve-3d;
+                  }
+                
+                .card-front,.card-back {
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    backface-visibility: hidden;
+                }
+            
+                .card-front {
+                    z-index: 1;
+                    transform: rotateY(0deg);
+
+                    h4 {
+                        position: relative;
+                        bottom: 5.5rem;
+                        left: 1rem;
+                        font-weight: bold;
+                        font-size: 1.6rem;
+                        width: 90%;
+                    }
+
+                }
+            
+                .card-back {
+                    background-color:#0384843d;
+                    color: white;
+                    transform: rotateY(180deg);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-radius: 0.7rem;
+
+                    .text-card-back {
+
+                        padding: 1rem;
+                        margin-top: 4rem;
+
+                        h4 {
+                            font-weight: bold;
+                            font-size: 1.6rem;
+                            width: 90%;
+                            text-align: start;
+                        }
+    
+                        p {
+                            text-align: start;
+                            font-weight: normal;
+                            font-size: large;
+                            margin-top: 4rem;
+                            margin-bottom: 2rem;
+                        }
+                    }
+
+                }
+            
+                &.flip {
+                    .card-inner {
+                        transform: rotateY(180deg);
+                    }
                 }
 
             }
